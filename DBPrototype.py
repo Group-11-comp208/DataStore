1a28eedbf8be6b3564d361b0c191aa9fff84d727
Ethan Halsall <ethan.halsall@gmail.com>
2021-04-14 20:41:18 +0100

    Fix types and clean up insert queries
    - Also make sure cursor is closed after use - if stays open - it can cause a memory leak....
    
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- DBPrototype.py
+++ DBPrototype.py
@@ -1,31 +1,30 @@
 import sqlite3
 from sqlite3 import Error
 
+
 class Database:
     def __init__(self, path):
         database = path
-        sql_create_chat_table = """ CREATE TABLE IF NOT EXISTS Chat (
+        sql_create_chat_table = """CREATE TABLE IF NOT EXISTS Chat (
                                             chatID integer PRIMARY KEY,
                                             blacklisted boolean NOT NULL,
-                                            preferences String
-                                        ); """
+                                            preferences TEXT
+                                        );"""
         sql_create_crypto_table = """CREATE TABLE IF NOT EXISTS CryptoCurrency (
-                                        name string PRIMARY KEY,
+                                        name TEXT PRIMARY KEY,
                                         price float NOT NULL,
                                         time date NOT NULL,
-                                        articleRanking String
+                                        articleRanking TEXT
                                     );"""
+
         # create a database connection
-        conn = self.create_connection(database)
+        self.conn = self.create_connection(database)
 
         # create tables
-        if conn is not None:
+        if self.conn is not None:
 
-            # create chat table
-            self.create_table(conn, sql_create_chat_table)
-
-            # create crypto table
-            self.create_table(conn, sql_create_crypto_table)
+            self.create_table(sql_create_chat_table)
+            self.create_table(sql_create_crypto_table)
 
         else:
             print("Error! cannot create the database connection.")
@@ -43,38 +42,39 @@ class Database:
 
         return conn
 
-    def create_table(self, conn, create_table_sql):
+    def create_table(self, create_table_sql):
         """ create a table from the create_table_sql statement
         :param conn: Connection objectpath"""
         try:
-            c = conn.cursor()
+            c = self.conn.cursor()
             c.execute(create_table_sql)
+            c.close()
         except Error as e:
             print(e)
 
     def insertIntoChat(self, chatID, blacklisted, preferences):
+        params = [chatID, blacklisted, preferences]
         try:
-            insertQuery = """INSERT INTO Chat (chatID,blacklisted,preferences)
-                             VALUES({},{},{});""".format(chatID, blacklisted, preferences)
+            insertQuery = "INSERT INTO Chat (chatID,blacklisted,preferences) VALUES(?,?,?)"
+            self._insert(insertQuery, params)
         except Error as e:
             print(e)
-
-        return insertQuery
 
     def insertIntoCrypto(self, name, price, time, articleRanking):
+        params = [name, price, time, articleRanking]
         try:
-            insertQuery = """INSERT INTO CryptoCurrency (name,price,time,articleRanking)
-                             VALUES({},{},{},{});""".format(name, price, time, articleRanking)
+            insertQuery = "INSERT INTO CryptoCurrency (name,price,time,articleRanking) VALUES(?,?,?,?)"
+            self._insert(insertQuery, params)
         except Error as e:
             print(e)
 
-        return insertQuery
-
-    def insert(self, conn, insertQuery):
-        cur = conn.cursor()
-        cur.execute(insertQuery)
-        conn.commit()
+    def _insert(self, insertQuery, params):
+        cur = self.conn.cursor()
+        cur.execute(insertQuery, params)
+        self.conn.commit()
+        cur.close()
         return cur.lastrowid
 
 
 database = Database("cryptoDB.db")
+database.insertIntoChat(101, False, 'ethan')
