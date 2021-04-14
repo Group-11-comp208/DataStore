93f499923303a708bf6141db840d4c6d8410bd40
Ethan Halsall <ethan.halsall@gmail.com>
2021-04-14 17:19:46 +0100

    Refactor code - convert database creator to a class
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- DBPrototype.py
+++ DBPrototype.py
@@ -6,59 +6,56 @@
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(db_file):
-    """create a database connection to the SQLite database
-       specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None"""
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
 
-    return conn
-    
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:"""
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+class Database:
+    def __init__(self, path):
+        database = path                           
+        sql_create_chat_table = """ CREATE TABLE IF NOT EXISTS Chat (
+                                            chatID integer PRIMARY KEY,
+                                            blacklisted boolean NOT NULL,
+                                            preferences String
+                                        ); """
+        sql_create_crypto_table = """CREATE TABLE IF NOT EXISTS CryptoCurrency (
+                                        name string PRIMARY KEY,
+                                        price float NOT NULL,
+                                        time date NOT NULL,
+                                        articleRanking String
+                                    );"""
+        #create a database connection    
+        conn = self.create_connection(database)
         
-def main():
-    database = r"cryptoDB.db"
-                                
-    sql_create_chat_table = """ CREATE TABLE IF NOT EXISTS Chat (
-                                        chatID integer PRIMARY KEY,
-                                        blacklisted boolean NOT NULL,
-                                        preferences String
-                                    ); """
-    
-    sql_create_crypto_table = """CREATE TABLE IF NOT EXISTS CryptoCurrency (
-                                    name string PRIMARY KEY,
-                                    price float NOT NULL,
-                                    time date NOT NULL,
-                                    articleRanking String
-                                );"""
-    
-    #create a database connection    
-    conn = create_connection(database)
-    
-    #create tables
-    if conn is not None:
-        
-        #create chat table
-        create_table(conn, sql_create_chat_table)
-        
-        #create crypto table
-        create_table(conn, sql_create_crypto_table)
-    else:
-        print("Error! cannot create the database connection.")   
+        #create tables
+        if conn is not None:
+            
+            #create chat table
+            self.create_table(conn, sql_create_chat_table)
+            
+            #create crypto table
+            self.create_table(conn, sql_create_crypto_table)
+        else:
+            print("Error! cannot create the database connection.")   
 
-if __name__ == '__main__':
-    main()
+    def create_connection(self, db_file):
+        """create a database connection to the SQLite database
+        specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None"""
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
+
+        return conn
+        
+    def create_table(self, conn, create_table_sql):
+        """ create a table from the create_table_sql statement
+        :param conn: Connection objectpath"""
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
+            
+
+database = Database("cryptoDB.db")
