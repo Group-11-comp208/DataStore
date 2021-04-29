4c7f322d5674b383f08447ffb91375f472ae8a9a
Ethan Halsall <ethan.halsall@gmail.com>
2021-04-29 14:43:52 +0100

    Add fx converer
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- web.py
+++ web.py
@@ -4,6 +4,7 @@ import json
 import time
 import requests
 import telegram
+from fx import Converter
 
 try:
     import thread
@@ -13,6 +14,7 @@ except ImportError:
 class CoinCapWebSocket:
     def __init__(self):
         self.database = Database()
+        self.converter = Converter()
         websocket.enableTrace(True)
         ws = websocket.WebSocketApp("wss://ws.coincap.io/prices?assets=ALL",
                                 on_open = self.on_open,
@@ -55,13 +57,18 @@ class CoinCapWebSocket:
         thread.start_new_thread(run, ())
 
     def telegram_bot_sendtext(self, chatID, name, change, value):
+        currency = self.database.get_currency(chatID)
+        currency_symbol = converter.get_symbol(currency)
+        rate = 1
+        if currency != "usd":
+            rate = self.converter.get_rate(currency)
         text = ""
         if (change > 0):
             text = "up"
         else:
             text = "down"
         bot = telegram.Bot(token='1543822532:AAEQJRD2-diWs0hCUSrVA8KqlDYkg-NV0_0')
-        bot.sendMessage(chat_id=chatID, text="*Price alert*\n{} is {} by {:.2f}% \nCurrent Price: ${}".format(name,text,change,value), parse_mode=telegram.ParseMode.MARKDOWN)
+        bot.sendMessage(chat_id=chatID, text="*Price alert*\n{} is {} by {:.2f}% \nCurrent Price: {}{}".format(name,text,change, currency_symbol,value * rate), parse_mode=telegram.ParseMode.MARKDOWN)
 
 web = CoinCapWebSocket()
 
