d1dc9f349149e8a397cfe04e5a2c934747ff251c
Ethan Halsall <ethan.halsall@gmail.com>
2021-04-29 15:38:47 +0100

    Make value a float
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- web.py
+++ web.py
@@ -68,7 +68,7 @@ class CoinCapWebSocket:
         else:
             text = "down"
         bot = telegram.Bot(token='1543822532:AAEQJRD2-diWs0hCUSrVA8KqlDYkg-NV0_0')
-        bot.sendMessage(chat_id=chatID, text="*Price alert*\n{} is {} by {:.2f}% \nCurrent Price: {}{}".format(name,text,change, currency_symbol,value * rate), parse_mode=telegram.ParseMode.MARKDOWN)
+        bot.sendMessage(chat_id=chatID, text="*Price alert*\n{} is {} by {:.2f}% \nCurrent Price: {}{}".format(name,text,change, currency_symbol, float(value) * rate), parse_mode=telegram.ParseMode.MARKDOWN)
 
 web = CoinCapWebSocket()
 
